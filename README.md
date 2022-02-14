71e498b71f7af1556878bf74cd21340b34fd0b5d
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-14 17:27:33 -0600

    my attempt at the challenge

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # todoListReact
-Created with CodeSandbox
+
+Practical implementation of
+
+1. React Hooks - useState()
+2. Event handling
+3. destructuring
+4. spread operator
+
+in a To-Do list project
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [listArray, setListArray] = useState([]);
+  const [list, setList] = useState("");
+
+  function listHandler(event) {
+    const { value } = event.target;
+    setList(value);
+  }
+
+  function todoHandler(event) {
+    setListArray((prevValue) => {
+      return [...prevValue, list];
+    });
+    setList("");
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" name="todo" onChange={listHandler} value={list} />
+        <button onClick={todoHandler}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {listArray.map((list) => (
+            <li>{list}</li>
+          ))}
         </ul>
       </div>
     </div>
