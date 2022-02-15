14d385a94eede0abb10e623d89911b0e5b1aacda
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-14 18:54:28 -0600

    component state

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ListItem(props) {
-  return <li>{props.item}</li>;
+  const [clickedState, setClickedState] = useState(false);
+  function clickHandler(event) {
+    clickedState ? setClickedState(false) : setClickedState(true);
+  }
+  return (
+    <li
+      onClick={clickHandler}
+      style={{ textDecoration: clickedState && "line-through" }}
+    >
+      {props.item}
+    </li>
+  );
 }
 
 export default ListItem;
